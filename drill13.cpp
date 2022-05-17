8bce0c70fd0b37f3ec4362a8d43c262dd035a791
tolvajpeter <90860692+tolvajpeter@users.noreply.github.com>
2022-05-17 06:48:58 +0200

    Update drill13.cpp

--- drill13.cpp
+++ drill13.cpp
@@ -1,42 +1,53 @@
-#include "Graph.h"
 #include "Simple_window.h"
-
-#include <string>
-#include <iostream>
-
-using namespace Graph_lib;
-int main(){
-	try {
-			Point t1(300, 50);
-			Simple_window win(t1, 1000, 800, "My window");
-			win.wait_for_button();
-			
-			Vector_ref <Rectangle>v;
-			for (int i=0; i<8; i++){
-				v.push_back(new Rectangle (Point(i*100, i*100), 100, 100));
-			v[v.size()-1].set_fill_color(Color::red);
-			win.attach(v[v.size()-1]);
-			}
-			win.wait_for_button();
-			
-			Lines grid;
-			int x_size = 800;
-			int y_size = 800;
-			for (int i=100; i<=x_size; i+=100) {
-					grid.add(Point(i,0), Point(i, y_size));
-					grid.add(Point(0,i), Point(x_size, i));
-			}
-			
-			win.attach(grid);
-			win.wait_for_button();
-	
-	
-	
-	} catch (exception& e) {
-			cerr << "exception: " << e.what() <<endl;
-			return 1;
-	} catch (...) {
-			cerr<< "exception\n";
-			return 2;
+#include "Graph.h"
+#include "std_lib_facilities.h"
+#include <utility>
+int main()
+{
+	using namespace Graph_lib;
+	Simple_window win {{100,100},1000,800,"Simple_window"};
+	int xmax = 800;
+	int ymax = 800;
+	int xgrid = 100;
+	int ygrid = 100;
+	Lines grid;
+	for (int x=xgrid; x<=xmax; x+=xgrid)
+		grid.add(Point{x,0},Point{x,ymax}); // vertical line
+	for (int y = ygrid; y<=ymax; y+=ygrid)
+		grid.add(Point{0,y},Point{xmax,y});	
+	Vector_ref<Rectangle> vr;
+	int valt = 0;
+	int bruh = 0;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			vr.push_back(new Rectangle{Point{bruh*100,valt*100},100,100});
+			bruh++;
+			valt++;
+			vr[vr.size()-1].set_fill_color(Color::red);
+			win.attach(vr[vr.size()-1]);
+		}
 	}
-	}
+	Image ii {Point{300,100},"badged.jpg"};
+	Image ii1 {Point{300,500},"badged.jpg"};
+	Image ii2 {Point{500,100},"badged.jpg"};
+	win.attach(ii);
+	win.attach(ii1);
+	win.attach(ii2);
+	win.attach(grid);
+	int kep_meret100=100;
+    Image ii3(Point(0,0),"badge.jpg");
+    ii3.set_mask(Point(200,0),kep_meret100,kep_meret100);
+    win.attach(ii3);
+    for (int i = 0; i < 8; ++i)
+    {
+    	for (int j = 0; j < 8; ++j)
+    	{
+    		win.wait_for_button();
+    		if (j < 7) ii3.move(100,0);
+    		else ii3.move(-700,100);
+    	}
+    }
+
+}
